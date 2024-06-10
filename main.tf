3460d4f41c294980e6a7152efe1d06a9b1432c4a
Apoorva Pandey <apoorvapandey@Apoorvas-MacBook-Air.local>
2024-06-10 13:54:02 +0530

    improved

--- main.tf
+++ main.tf
@@ -1,73 +1,102 @@
 provider "aws" {
-      region = "us-east-1"
-    }
+  region = "us-east-1"
+}
 
-    resource "aws_vpc" "main" {
-      cidr_block = "10.0.0.0/16"
-    }
+data "aws_vpc" "default" {
+  default = true
+}
 
-    resource "aws_subnet" "subnet" {
-      vpc_id     = aws_vpc.main.id
-      cidr_block = "10.0.1.0/24"
-    }
+data "aws_subnet" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 
-    resource "aws_security_group" "ecs" {
-      vpc_id = aws_vpc.main.id
+  filter {
+    name   = "availability-zone"
+    values = ["us-east-1a"]
+  }
 
-      ingress {
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+  filter {
+    name   = "default-for-az"
+    values = ["true"]
+  }
+}
+
+data "aws_security_group" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+
+  filter {
+    name   = "group-name"
+    values = ["default"]
+  }
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
       }
+    ]
+  })
+}
 
-      egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-      }
-    }
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
 
-    resource "aws_ecs_cluster" "main" {
-      name = "hello-world-cluster"
-    }
+resource "aws_ecs_cluster" "main" {
+  name = "hello-world-cluster"
+}
 
-    resource "aws_ecs_task_definition" "hello_world" {
-      family                   = "hello-world-task"
-      network_mode             = "awsvpc"
-      requires_compatibilities = ["FARGATE"]
-      cpu                      = "256"
-      memory                   = "512"
+resource "aws_ecs_task_definition" "hello_world" {
+  family                   = "hello-world-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
-      container_definitions = jsonencode([{
-        name      = "hello-world-container"
-        image     = "268724/hello-world-nodejs:latest"
-        essential = true
-        portMappings = [{
-          containerPort = 3000
-          hostPort      = 3000
-        }]
-      }])
-    }
+  container_definitions = jsonencode([{
+    name      = "hello-world-container"
+    image     = "268724/hello-world-nodejs:latest"
+    essential = true
+    portMappings = [{
+      containerPort = 3000
+      hostPort      = 3000
+    }]
+  }])
+}
 
-    resource "aws_ecs_service" "main" {
-      name            = "hello-world-service"
-      cluster         = aws_ecs_cluster.main.id
-      task_definition = aws_ecs_task_definition.hello_world.arn
-      desired_count   = 1
-      launch_type     = "FARGATE"
-      
-      network_configuration {
-        subnets          = [aws_subnet.subnet.id]
-        security_groups  = [aws_security_group.ecs.id]
-      }
-    }
+resource "aws_ecs_service" "main" {
+  name            = "hello-world-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.hello_world.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  
+  network_configuration {
+    subnets          = [data.aws_subnet.default.id]
+    security_groups  = [data.aws_security_group.default.id]
+    assign_public_ip = true
+  }
+}
 
-    output "cluster_name" {
-      value = aws_ecs_cluster.main.name
-    }
+output "cluster_name" {
+  value = aws_ecs_cluster.main.name
+}
 
-    output "service_name" {
-      value = aws_ecs_service.main.name
-    }
+output "service_name" {
+  value = aws_ecs_service.main.name
+}
